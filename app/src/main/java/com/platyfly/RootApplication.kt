e6f7f5fbb356885ce7d769eb3f6c3ee4aca04817
Bradley Wersterfer <Bradley.Wersterfer@utdallas.edu>
2024-01-02 17:48:58 -0500

    Who knows if this works? Not my 'invalid' cache

---
+++ app/src/main/java/com/platyfly/RootApplication.kt
@@ -0,0 +1,8 @@
+package com.platyfly
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RootApplication : Application() {
+}
---
+++ app/src/main/java/com/platyfly/player/InputControls.kt
@@ -0,0 +1,6 @@
+package com.platyfly.player
+
+interface InputControls {
+    fun left(): Float
+    fun right(): Float
+}
