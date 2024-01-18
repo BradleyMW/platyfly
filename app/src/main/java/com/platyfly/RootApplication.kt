fa3a91a7eaf87bf1ed0fa68a727addb7a12038c2
Bradley Wersterfer <Bradley.Wersterfer@utdallas.edu>
2024-01-17 22:18:06 -0500

    Added dep injection via Dagger and hilt, fixed build system by upgrading Gradle to 7.4 and JDK17, enabled viewBindings and added first pass on player classes

---
+++ app/src/main/java/com/platyfly/RootApplication.kt
@@ -0,0 +1,7 @@
+package com.platyfly
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RootApplication : Application()
---
+++ app/src/main/java/com/platyfly/player/Player.kt
@@ -0,0 +1,59 @@
+package com.platyfly.player
+
+import android.view.SurfaceView
+import java.util.concurrent.atomic.AtomicBoolean
+import javax.inject.Inject
+
+class Player @Inject constructor(inputControls: InputControls) : Runnable {
+    // Interface to the player's movement system
+    val input: InputControls = inputControls
+
+    // The rendered image and its location on the screen
+    lateinit var image: SurfaceView
+
+    var running: AtomicBoolean = AtomicBoolean(false)
+
+    /**
+     * When an object implementing interface `Runnable` is used
+     * to create a thread, starting the thread causes the object's
+     * `run` method to be called in that separately executing
+     * thread.
+     *
+     *
+     * The general contract of the method `run` is that it may
+     * take any action whatsoever.
+     *
+     * @see java.lang.Thread.run
+     */
+    override fun run() {
+        running.set(true)
+        while (running.get()) {
+            updatePosition()
+            animate()
+        }
+    }
+
+    /**
+     * Wraps logic around updating the position of the player based on the configured
+     * input control system.
+     */
+    private fun updatePosition() {
+        // Update the x coordinate to its right - left input difference
+        val newX = image.x + (input.right() - input.left())
+        image.x = Math.max(0.0f, newX)
+    }
+
+    private fun animate() {
+        if (image.holder.surface.isValid) {
+        }
+        image.invalidate()
+    }
+
+    /**
+     * Call to gracefully shut down the player and clean up any other resources.
+     */
+    fun shutdown() {
+        running.set(false)
+        image.holder.surface.release()
+    }
+}
